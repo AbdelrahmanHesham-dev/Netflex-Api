ccd09166e82220589f705dae01f657ffd00bafb6
Abdelrahman <abdelrahmanhesham477@gmail.com>
2024-10-05 04:37:46 +0300

    update

--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,28 @@
 let submitBtn = document.getElementById("submitBtn");
 
-submitBtn.disabled = true;
+submitBtn.style.pointerEvents = "none";
+
 function valid() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    let emailError = document.getElementById("emailError")
-    let passwordError = document.getElementById("passwordError")
+    let emailError = document.getElementById("emailError");
+    let passwordError = document.getElementById("passwordError");
+
     if (email.indexOf("@") == -1 || email.length <= 0) {
-        emailError.innerHTML = "please Enter a Valid Email"
-        submitBtn.disabled = true;
-    }else if (email.indexOf("@") > -1 && email.length > 0) {
-        emailError.innerHTML = ""
+        emailError.innerHTML = "Please Enter a Valid Email";
+        submitBtn.style.pointerEvents = "none"; 
+    } else if (email.indexOf("@") > -1 && email.length > 0) {
+        emailError.innerHTML = "";
     }
-    if(password.length < 8 || password.length > 25) {
+
+    if (password.length < 8 || password.length > 25) {
         passwordError.innerHTML = "Please enter a strong password greater than 8 characters.";
-        submitBtn.disabled = true;
-    } else if(password.length > 8 || password.length < 25) {
+        submitBtn.style.pointerEvents = "none"; 
+    } else if (password.length >= 8 && password.length <= 25) {
         passwordError.innerHTML = "";
     }
 
-    if (email.indexOf("@") > -1 && email.length > 0 && password.length >= 8 && password.length < 25){
-        submitBtn.disabled = false;
-
+    if (email.indexOf("@") > -1 && email.length > 0 && password.length >= 8 && password.length <= 25) {
+        submitBtn.style.pointerEvents = "auto"; 
     }
 }
